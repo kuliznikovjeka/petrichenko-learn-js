731d8a83e8ee1d1bd477f016a3ccd1cc7d616718
jeka <99824934+kuliznikovjeka@users.noreply.github.com>
2023-04-13 12:16:13 +0300

    praktica3

--- lesson1.js
+++ lesson1.js
@@ -1,6 +1,16 @@
 'use strict'
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+	}
+}
+start();
+
 const personalMovieDB = {
 	count: numberOfFilms,
 	movies: {},
@@ -9,31 +19,48 @@ const personalMovieDB = {
 	privat: false
 };
 
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		const a = prompt('Один из последних просмотренных фильмов?', ''),
+			b = prompt('На сколько оцените его?', '');
 
-for (let i = 0; i < 2; i++) {
-	const a = prompt('Один из последних просмотренных фильмов?', ''),
-		b = prompt('На сколько оцените его?', '');
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log('done');
+		} else {
+			console.log('error');
+			i--; //возвращает на 1 цикл назад
+		}
 
-	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-		personalMovieDB.movies[a] = b;
-		console.log('done');
-	} else {
-		console.log('error');
-		i--; //возвращает на 1 цикл назад
 	}
-
+	// a != null - означает, что пользователь не должен нажать "отмена"
 }
-// a != null - означает, что пользователь не должен нажать "отмена"
+rememberMyFilms();
 
+function detectPersonalLevel() {
+	if (personalMovieDB.count < 10) {
+		console.log("Просмотрено довольно мало фильмов");
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log("Вы классический зритель");
+	} else if (personalMovieDB.count >= 30) {
+		console.log("Вы киноман");
+	} else {
+		console.log("Произошла ошибка");
+	}
+};
+detectPersonalLevel()
 
-if (personalMovieDB.count < 10) {
-	console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-	console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-	console.log("Вы киноман");
-} else {
-	console.log("Произошла ошибка");
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
 }
+showMyDB(personalMovieDB.privat);
 
-console.log(personalMovieDB);
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+	}
+}
+writeYourGenres();
+
